903cea6fa54127cda61a9154774471218a879e87
Thompson Hall <Quaaysan@gmail.com>
2024-03-26 17:13:37 +0000

    Chapter 2 baby

--- Start/Ch_2/deque.py
+++ Start/Ch_2/deque.py
@@ -5,11 +5,17 @@ import string
 
 
 # TODO: initialize a deque with lowercase letters
-
+d = collections.deque('abcdefg')
 # TODO: deques support the len() function
-
+print(len(d))
 # TODO: deques can be iterated over
-
+for letter in d:
+    print(letter.upper())
 # TODO: manipulate items from either end
-
+d.pop()
+d.popleft()
+d.append("<")
+d.appendleft(">")
+d.rotate(1)
 # TODO: use an index to get a particular item
+print(d[5])
