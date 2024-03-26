1119b65ef07b65a1d3ea75cd994c2043eabd5ccb
Thompson Hall <Quaaysan@gmail.com>
2024-03-26 19:43:39 +0000

    Chapter2 finished

--- Start/Ch_2/challenge_start.py
+++ Start/Ch_2/challenge_start.py
@@ -2,8 +2,16 @@
 # Programming challenge: use advanced data collections on the earthquake data
 
 import json
-
+from collections import defaultdict
 
 # open the data file and load the JSON
-with open("../../30DayQuakes.json", "r") as datafile:
+with open("30DayQuakes.json", "r") as datafile:
     data = json.load(datafile)
+
+summary = defaultdict(int)
+for feature in data["features"]:
+    summary[feature["properties"]["type"]] += 1
+
+#print(summary)
+for key, value in summary.items():
+    print(key, ":", value)
